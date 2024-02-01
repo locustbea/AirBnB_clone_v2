7cb811fb7603be5a4c15d4a69a3b40f854659f32
locustbea <locustbea@yahoo.com>
2024-01-31 20:38:43 -0800

    more deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.operations import run, put, sudo, local
 from datetime import datetime
 import os
 
-env.hosts = ['66.70.184.249', '54.210.138.75']
+env.hosts = ['18.206.197.234', '18.234.129.56']
 created_path = None
 
 
